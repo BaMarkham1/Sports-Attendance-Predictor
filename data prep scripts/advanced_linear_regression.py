a9daa1bdf0d00335feb38f40927f8114b5c3b4c7
BaMarkham1 <benjamin.lemarc@ucdenver.edu>
2020-04-22 21:47:32 -0600

    extra data

--- data prep scripts/advanced_linear_regression.py
+++ data prep scripts/advanced_linear_regression.py
@@ -1,19 +1,11 @@
 # -*- coding: utf-8 -*-
 import pandas as pd
 import numpy as np
-from sklearn.model_selection import train_test_split 
 from sklearn.linear_model import LinearRegression
-from sklearn.decomposition import PCA
 
-def prepare_x_y(dataset, target_column, columns_to_drop):
+def prepare_x_y(dataset, target_column, x_columns):
     #get the target column to predict
     y = dataset[target_column].to_numpy()
-    #get all the current columns
-    x_columns = list(dataset.columns)
-    #drop the columns we don't want to use as independent variables
-    x_columns.remove(target_column)
-    for col in columns_to_drop:
-        x_columns.remove(col)
     #get a numpy array from the dataframe
     x = dataset[x_columns].to_numpy()
     return x, y
@@ -24,7 +16,9 @@ def do_linear_regression(x_train, x_test, y_train):
     regressor.fit(x_train, y_train)
     #predict based on the training data
     y_pred = regressor.predict(x_test)
-    return y_pred
+    coefs = [regressor.intercept_]
+    coefs.extend(regressor.coef_)
+    return y_pred, coefs
 
 def get_train_and_test_sets(splits, test_index):
     train_list = splits.copy()
@@ -38,24 +32,35 @@ def cross_validation(x, y, num_of_folds):
     total_actual_list = []
     x_splits = np.array_split(x, num_of_folds)
     y_splits = np.array_split(y, num_of_folds)
+    coef_list = []
     for split_index in range(num_of_folds):
         x_train, x_test = get_train_and_test_sets(x_splits, split_index)
         y_train, y_test = get_train_and_test_sets(y_splits, split_index)
-        y_pred = do_linear_regression(x_train, x_test, y_train)
+        y_pred, coefficients = do_linear_regression(x_train, x_test, y_train)
         #get the correlation between predicted and actual values
         print(np.corrcoef(y_test, y_pred)[0][1])
         total_pred_list.extend(y_pred.tolist())
         total_actual_list.extend(y_test.tolist())
+        coef_list.append(coefficients)
     total_pred = np.asarray(total_pred_list)
     total_actual = np.asarray(total_actual_list)
-    return np.corrcoef(total_actual, total_pred)[0][1]
+    return total_actual, total_pred, coef_list
+    #return np.corrcoef(total_actual, total_pred)[0][1]
     
     
 #read in the dataset
-dataset = pd.read_csv("../csvs/full_dataset.csv")
+dataset = pd.read_csv("../csvs/normalized.csv")
 #list columns to not be an x
-x_columns_to_drop = ["year", "team", "year 1 attend%", "attend%", "attendance"]
-x, y = prepare_x_y(dataset, "year 1 attendance", x_columns_to_drop)
-print(cross_validation(x, y, 5))
+x_columns = ["probowler_count", "win%", "division", "playoffs", "capacity",	"climate"]
+x, y = prepare_x_y(dataset, "year 1 attendance", x_columns)
+y_actual, y_pred, coef_list = cross_validation(x, y, 5)
+dataset["predicted"] = y_pred
+dataset["difference"] = y_actual - y_pred
+x_columns.insert(0, "intercept")
+coefficient_df = pd.DataFrame(coef_list, columns=x_columns)
+coefficient_df.loc['mean'] = coefficient_df.mean()
+coefficient_df.to_csv("../csvs/coefficients.csv")
+dataset = dataset.sort_values(by='difference', ascending=False)
+dataset.to_csv("../csvs/post_regression_data.csv", index=False)
 
 
--- data prep scripts/merge_data_files.py
+++ data prep scripts/merge_data_files.py
@@ -25,13 +25,37 @@ def get_diff_season_stats(df, stat_list, direction):
             for stat in stat_list: 
                 df.at[index, "year " + str(direction) + " " + stat] = df.at[match_index, stat]
     return df
+
+
+#Subtract the mean from each value and divide the result by the std deviation of the column
+def normalize_data(data_array):
+    mean_array = np.mean(data_array, axis = 0)
+    std_array = np.std(data_array, axis = 0)
+    normalized_array = np.zeros(data_array.shape)
+    for col_index, col in enumerate(mean_array):
+        mean = mean_array[col_index]
+        std = std_array[col_index]
+        normalized_array[:,col_index] = (data_array[:,col_index] - mean) / std
+    return normalized_array
         
 def remove_blanks(df):
     df.replace('', np.nan, inplace=True)
     df = df.dropna()
     return df
     
+def normalize_table(df):
+    #print(df.mean())
+    print(df.std())
+    normalized_df=(df-df.mean())/df.std()
+    columns_to_keep = ["attendance", "attend%", "year 1 attend%", "year 1 attendance"]
+    for col in columns_to_keep:
+        normalized_df[col] = df[col]
+    return normalized_df
+    
+
 merged_table = merge_data(probowl_count, team_record, attendance)
 next_year_df = get_diff_season_stats(merged_table, ["attend%", "attendance"], 1)
 next_year_df = remove_blanks(next_year_df)
+next_year_df = normalize_table(next_year_df)
 next_year_df.to_csv("../csvs/full_dataset.csv")
+normalize_table(next_year_df).to_csv("../csvs/normalized.csv")
